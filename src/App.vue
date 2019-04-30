e4d3bf20a79502ad9ad88df2124d4ea412a457fb
Richard Lindsay <richard@tictocfamily.com>
2019-04-30 15:58:16 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GoogleMap />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GoogleMap from './components/GoogleMap.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    GoogleMap
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html, body, #app {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+  }
+
 </style>
---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="google_map"></div>
+</template>
+
+<script>
+  export default {
+    name: 'GoogleMap',
+    data: function() {
+      return {
+        taprooms: [
+          {
+            "name": "Wylam Brewery",
+            "location": {lat: 54.985925, lng: -1.614847}
+          },
+          {
+            "name": "Wylam Brewery",
+            "location": {lat: 53.475139, lng: -2.221963}
+          }
+        ]
+      }
+    },
+    mounted: function () {
+      const map = new google.maps.Map(document.getElementById('google_map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 12
+      });
+
+      for (var i = 0; i < this.taprooms.length; i++) {
+        var marker = new google.maps.Marker({
+          position: this.taprooms[i].location,
+          map: map,
+          title: this.taprooms[i].name
+        });
+      }
+
+      // navigator.geolocation.getCurrentPosition(function(position) {
+      //   var pos = {
+      //     lat: position.coords.latitude,
+      //     lng: position.coords.longitude
+      //   };
+
+      //   map.setCenter(pos)
+      // });
+    }
+  };
+</script>
+
+<style scoped>
+  #google_map {
+    width: 100%;
+    height: 100%;
+  }
+</style>
